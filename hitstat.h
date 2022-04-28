5a95314fff6a492e7c3b7831739bfe158300719b
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-04-28 14:04:40 +0300

    add examples

--- hitstat.h
+++ hitstat.h
@@ -4,6 +4,16 @@
 /* zero-overhead hitcounter/meancounters
 hit(conditions...) record percent of condition==true for each condition
 mean(values...) record min/average/max of values
+examples:
+insertion: if(a>mean(d) && hit(a>b))
+will result in `if(a>d && (a>b))
+with (a>b) condition being recorded
+and (d) being sampled for min/max/avg
+collection:
+hit(a>b,b<c,d>b); will record true% of conditions in sequence
+mean(a,b,c); will record min/max/avg of a,b,c
+
+
 */
 //https://github.com/FrozenVoid/hitstat/
 
